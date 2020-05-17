e7163fe207384df7bb26db505b3f207b8203da2e
Giulai <giu.delci@gmail.com>
2020-05-17 12:16:22 +0200

    prove

--- walkornot.js
+++ walkornot.js
@@ -1,3 +1,5 @@
+let sensor = new Accelerometer({frequency: 1});
+
 function getdata(){
 	var prec=0;
 	var act=0;
@@ -5,7 +7,7 @@ function getdata(){
 	var i=0;
 	var d =0;
 	var r=0;
-	let sensor = new Accelerometer({frequency: 1});
+	
 	sensor.start();
 	sensor.onreading = () => {
 		l[0]= sensor.x.toFixed(2) ;
@@ -15,10 +17,10 @@ function getdata(){
 		act=Math.sqrt(l[0]^2 + l[1]^2 + l[2]^2 ).toFixed(2);
 		
 		d = Math.abs(act - prec).toFixed(2);
-		if ( d > 9){
+		if ( d > 0.9){
 			i = i+1;
 			document.getElementById('status_act').innerHTML = "Activity: RUNNING w:  " + i  + " r: " + r ;
-		}else if (d > 5){
+		}else if (d > 0.5){
 			r=r+1;
 			document.getElementById('status_act').innerHTML = "Activity: WALKING w:  " + i + " r: " + r ;
 		}else{
@@ -30,4 +32,8 @@ function getdata(){
 		document.getElementById('status').innerHTML = "Error Name" + event.error.name + "Error Message " + event.error.message;
 	};
 
+}
+
+function stop(){
+	sensor.stop()
 }
